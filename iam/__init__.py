552a214aaf51d83e572d47c38fa0ffd5d2b6ec58
aaronlelevier <aaron.lelevier@gmail.com>
2019-06-09 14:44:03 -0700

    implement Policy class with tests, refactor classes

--- iam/__init__.py
+++ iam/__init__.py
@@ -5,26 +5,34 @@ ACCOUNT_ID = '123456789'
 
 class AbstractResource(abc.ABC):
     @abc.abstractproperty
-    def resource_type(self):
+    def service(self):
         pass
 
-
-class IamResourceMixin:
-    service = 'iam'
-
-
-class AbstractIamResource(IamResourceMixin, AbstractResource):
-    pass
-
-
-class Role(AbstractIamResource):
-    def __init__(self, name):
-        self.name = name
-
-    @property
+    @abc.abstractproperty
     def resource_type(self):
-        return 'role'
+        pass
 
     @property
     def arn(self):
         return f'arn:aws:{self.service}::{ACCOUNT_ID}:{self.resource_type}/{self.name}'
+
+
+class AbstractIamResource(AbstractResource):
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def service(self):
+        return 'iam'
+
+
+class Role(AbstractIamResource):
+    @property
+    def resource_type(self):
+        return 'role'
+
+
+class Policy(AbstractIamResource):
+    @property
+    def resource_type(self):
+        return 'policy'
--- tests/test_iam.py
+++ tests/test_iam.py
@@ -1,6 +1,6 @@
 import unittest
 
-from iam import Role
+from iam import Role, Policy
 
 
 class RoleTests(unittest.TestCase):
@@ -12,3 +12,14 @@ class RoleTests(unittest.TestCase):
         ret = role.arn
 
         assert ret == 'arn:aws:iam::123456789:role/rds-monitoring-role'
+
+
+class PolicyTests(unittest.TestCase):
+
+    def test_arn(self):
+        name = 'DenyIAMAccess'
+        role = Policy(name=name)
+
+        ret = role.arn
+
+        assert ret == 'arn:aws:iam::123456789:policy/DenyIAMAccess'
