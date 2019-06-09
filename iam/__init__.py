69b1cc5dc3ebe09cc1a82c04ea10d9db2355a51f
aaronlelevier <aaron.lelevier@gmail.com>
2019-06-09 14:27:39 -0700

    added 'AbstractIamResource' with 'IamResourceMixin'

--- iam/__init__.py
+++ iam/__init__.py
@@ -1,7 +1,30 @@
-class Role:
+import abc
+
+ACCOUNT_ID = '123456789'
+
+
+class AbstractResource(abc.ABC):
+    @abc.abstractproperty
+    def resource_type(self):
+        pass
+
+
+class IamResourceMixin:
+    service = 'iam'
+
+
+class AbstractIamResource(IamResourceMixin, AbstractResource):
+    pass
+
+
+class Role(AbstractIamResource):
     def __init__(self, name):
         self.name = name
 
     @property
+    def resource_type(self):
+        return 'role'
+
+    @property
     def arn(self):
-        return f'arn:aws:iam::123456789:role/{self.name}'
+        return f'arn:aws:{self.service}::{ACCOUNT_ID}:{self.resource_type}/{self.name}'
